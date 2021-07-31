a88f6419617952ca69fb851bf7bd8cf140614ef7
shree <rushikeshwankhade17@gmail.com>
2021-07-31 21:28:58 +0530

    commit initial

--- src/main/java/com/example/demo/GitPractiseApplication.java
+++ src/main/java/com/example/demo/GitPractiseApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitPractiseApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitPractiseApplication.class, args);
+		ConfigurableApplicationContext ctxt = SpringApplication.run(GitPractiseApplication.class, args);
+		
+		
+		String name =  "This is Rushi System";
+		System.out.println(name);
+		
 	}
 
 }
